070c3aa851c138626d4317922d7e2c94c56d2ca6
ArthurMartin26 <arthur.martin@dhsc.gov.uk>
2026-02-13 16:29:25 +0000

    dded packages package

---
+++ Code/R/packages and functions.R
@@ -0,0 +1,10 @@
+
+library(haven)     # read Stata .dta
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(labelled)
+library(purrr)
+library(survey)    # weighted H, A, M0
+library(readr)
+library(haven)
